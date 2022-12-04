acc870e260c9d5c4bef14c1ef4f9d1eb50132c54
Daniel Jašek <jasekdan@gmail.com>
2022-12-04 23:13:58 +0100

    complete day 4

---
+++ 2022/4/src/ElfDuty.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace AdventOfCode._2022.Day4;
+
+public class ElfDuty
+{
+    public ICollection<int> Floors { get; }
+
+    public ElfDuty(string input)
+    {
+        Floors = new List<int>();
+        var floorsMinMax =input.Split('-');
+
+        var floorsMin = int.Parse(floorsMinMax[0]);
+        var floorsMax = int.Parse(floorsMinMax[1]);
+
+        for (var floor = floorsMin; floor <= floorsMax; floor++)
+        {
+            Floors.Add(floor);
+        }
+
+    }
+}
---
+++ 2022/4/src/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode._2022.Day4;
+
+public class Program
+{
+    public static int Main()
+    {
+        using var fs = new FileStream(Path.Combine(Directory.GetCurrentDirectory(), "Input.txt"), FileMode.Open);
+        using var sr = new StreamReader(fs);
+
+        var completeIntersect = 0;
+        var partialIntersect = 0;
+
+        while (!sr.EndOfStream)
+        {
+            var input = sr.ReadLine()!.Split(',');
+
+            var firstDuty = new ElfDuty(input[0]);
+            var secondDuty = new ElfDuty(input[1]);
+
+            if (firstDuty.Floors.Intersect(secondDuty.Floors).Count() == firstDuty.Floors.Count
+                || secondDuty.Floors.Intersect(firstDuty.Floors).Count() == secondDuty.Floors.Count)
+            {
+                completeIntersect++;
+            }
+
+            if (firstDuty.Floors.Intersect(secondDuty.Floors).Count() != 0)
+            {
+                partialIntersect++;
+            }
+        }
+        
+        Console.WriteLine(completeIntersect);
+        Console.WriteLine(partialIntersect);
+        return 0;
+    }
+}
